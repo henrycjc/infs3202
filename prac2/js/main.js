e14d9afd6872516756de3cf9de1616ed9ddc932a
Henry Chladil <henry.ponco@gmail.com>
2015-03-20 14:00:45 +1000

    Begin Prac 2 and removed duplicate images

---
+++ prac2/js/main.js
@@ -0,0 +1,38 @@
+function initialize() {
+    var res1 = new google.maps.LatLng(-27.505633, 152.960994); //royal thai
+    var res2 = new google.maps.LatLng(-27.501785, 152.974403); //laksa hut
+    var res3 = new google.maps.LatLng(-27.492614, 152.979461); // dos amigos
+    var res4 = new google.maps.LatLng(-27.501400, 152.972454); // Harajuku Gyoza
+
+    var mapOptions = {
+        center: { lat: -27.500580, lng: 152.971195}, //-27.500580, 152.971195
+        zoom: 15
+    };
+    var marker1 = new google.maps.Marker({
+        position: res1,
+        icon: "../img/maps/MarkerA.png",
+        title:"Royal Sri Thai"
+    });
+    var marker2 = new google.maps.Marker({
+        position: res2,
+        icon: "../img/maps/MarkerB.png",
+        title:"Laksa Hut"
+    });
+    var marker3 = new google.maps.Marker({
+        position: res3,
+        icon: "../img/maps/MarkerC.png",
+        title:"Dos Amigos"
+    });
+    var marker4 = new google.maps.Marker({
+        position: res4,
+        icon: "../img/maps/MarkerD.png",
+        title:"Harajuku Gyoza"
+    });
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    marker1.setMap(map);
+    marker2.setMap(map);
+    marker3.setMap(map);
+    marker4.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
